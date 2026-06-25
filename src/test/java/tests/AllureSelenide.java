ac754951fee8001b2074f0d7a27df8ba1ea0a000
Артем Избышев <izbyshev-aa@mail.ru>
2026-06-26 01:59:24 +0300

    Добавил аннотации для аллюра и начал делать pageObject

---
+++ src/test/java/tests/AllureSelenide.java
@@ -0,0 +1,16 @@
+package tests;
+
+import com.codeborne.selenide.logevents.LogEvent;
+import com.codeborne.selenide.logevents.LogEventListener;
+
+public class AllureSelenide implements LogEventListener {
+    @Override
+    public void afterEvent(LogEvent currentLog) {
+
+    }
+
+    @Override
+    public void beforeEvent(LogEvent currentLog) {
+
+    }
+}
